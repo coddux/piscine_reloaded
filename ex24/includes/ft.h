fecea54b61526acd92e48c2a9cdd00ebfe3f9d9a
Alexandra Lepercq <alepercq@z3r7p1.le-101.fr>
2018-10-16 19:42:45 +0200

    ex24

---
+++ ex24/includes/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft.h                                             .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: alepercq <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/03 12:40:29 by alepercq     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/03 12:41:24 by alepercq    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
+
+#endif
