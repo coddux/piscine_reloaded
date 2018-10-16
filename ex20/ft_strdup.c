30397ec9e52d5d86d093f0ea41b1e9831efdfd55
Alexandra Lepercq <alepercq@z3r7p1.le-101.fr>
2018-10-16 19:41:25 +0200

    ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strdup.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: alepercq <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/03 10:44:10 by alepercq     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/03 16:00:08 by alepercq    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i = i + 1;
+	}
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *mem;
+
+	if (!(mem = (char*)malloc(sizeof(*mem) * (ft_strlen(src) + 1))))
+		return (0);
+	ft_strcpy(mem, src);
+	return (mem);
+}
